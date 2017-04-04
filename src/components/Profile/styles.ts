037684a62139c73bddd951a954fc4c66d9f0d6c1
Jacob Froman <jacob.h.froman@gmail.com>
2017-04-03 20:13:53 -0500

    Wired up profile

---
+++ src/components/Profile/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+import parentStyles from '../styles';
+
+const styles = StyleSheet.create({});
+
+export default {
+    ...parentStyles,
+    ...styles
+};
--- src/components/RuffLove.tsx
+++ src/components/RuffLove.tsx
@@ -4,10 +4,12 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import reducer from '../state/reducers';
-import { fetchPet } from '../state/actions';
-import Main from './Main/Main';
 import { RootRoute } from '../config/routes';
+import { fetchPet } from '../state/actions';
+import reducer from '../state/reducers';
+import Main from './Main/Main';
+import Profile from './Profile';
+
 
 const loggerMiddleware = createLogger();
 
@@ -21,14 +23,16 @@ class RuffLove extends Component<IRuffLoveProps, IRuffLoveState> {
     componentDidMount() {
         // Fetch 2 pets on initial app load. TODO: Revisit this if/when getting sequential pets is
         // unwanted behavior
-        store.dispatch(fetchPet(store.getState().offset, '55401'));
-        store.dispatch(fetchPet(store.getState().offset + 1, '55401'));
+        store.dispatch(fetchPet(store.getState().offset, store.getState().postalCode));
+        store.dispatch(fetchPet(store.getState().offset + 1, store.getState().postalCode));
     }
 
     render() {
         const Nav = TabNavigator({
-            [RootRoute.Main]: { screen: Main }
-        });
+            [RootRoute[RootRoute.Main]]: { screen: Main },
+            [RootRoute[RootRoute.Profile]]: { screen: Profile }
+        },
+            { initialRouteName: 'Profile' });
         return <Provider store={store}><Nav {...this.props} /></Provider>;
     }
 }
