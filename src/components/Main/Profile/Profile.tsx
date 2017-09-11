13924e856288db5dc040c8d339da17e8e3730fcb
Jacob Froman <jacob.h.froman@gmail.com>
2017-09-10 21:03:05 -0500

    Add logout button to profile

--- src/components/Main/Profile/Profile.tsx
+++ src/components/Main/Profile/Profile.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { View, Switch } from 'react-native';
-import { TextInput } from '../../common';
+import { TextInput, Button, Text } from '../../common';
 import styles from './styles';
 
 interface IProfileProps {
     postalCode: string;
     onChangePostalCode: (text: string) => void;
     onChangePetType: () => void;
+    onLogoutPress: () => void;
     petType: 'dog' | 'cat';
 }
 
 function Profile(props: IProfileProps) {
     return (
         <View style={styles.container}>
-            <TextInput
-                placeholder='Postal Code'
-                onChangeText={props.onChangePostalCode}
-                value={props.postalCode}
-            />
+            <TextInput placeholder="Postal Code" onChangeText={props.onChangePostalCode} value={props.postalCode} />
             <Switch onValueChange={props.onChangePetType} value={props.petType === 'dog'} />
+            <Button onPress={props.onLogoutPress}>
+                <Text.Description>Logout</Text.Description>
+            </Button>
         </View>
     );
 }
